b02f89edc1595f68c69f4b7ece50ea5b393c2653
Artem Kolin <artemkaxboy@gmail.com>
2020-01-20 21:16:05 +0700

    testify added

--- configuration_test.go
+++ configuration_test.go
@@ -2,6 +2,7 @@ package configuration
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
 	"runtime"
 	"sync"
 	"testing"
@@ -18,22 +19,19 @@ func TestParseKeyOrder(t *testing.T) {
 		}()
 
 		for i := 0; i < 1000; i++ {
-			conf, err1 := LoadConfig("tests/configs.conf")
-			if err1 != nil {
-				t.Error(err1)
-			}
-			for g := 1; g < 3; g++ {
-				for i := 1; i < 4; i++ {
-					key := fmt.Sprintf("test.out.a.b.c.d.groups.g%d.o%d.order", g, i)
-					order, err2 := conf.GetInt32(key, -1)
-					if err2 != nil {
-						t.Error(err2)
-					}
-
-					if order != int32(i) {
-						fmt.Println(conf)
-						t.Fatalf("order not match,group %d, except: %d, real order: %d", g, i, order)
-						return
+			conf, err := LoadConfig("tests/configs.conf")
+			if assert.Nil(t, err) {
+				for g := 1; g < 3; g++ {
+					for i := 1; i < 4; i++ {
+						key := fmt.Sprintf("test.out.a.b.c.d.groups.g%d.o%d.order", g, i)
+						order, err := conf.GetInt32(key, -1)
+						if assert.Nil(t, err) {
+							if order != int32(i) {
+								fmt.Println(conf)
+								t.Fatalf("order not match,group %d, except: %d, real order: %d", g, i, order)
+								return
+							}
+						}
 					}
 				}
 			}
